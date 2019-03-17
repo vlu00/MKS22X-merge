32f9689004ed49f9153f0d289cd9ea92c02bc073
vlu00 <vlu00@stuy.edu>
2019-03-16 22:25:50 -0400

    Tried mergesort. Got stackoverflow error

--- Merge.java
+++ Merge.java
@@ -3,7 +3,22 @@ import java.io.*;
 import java.util.*;
 
 public class Merge {
-  public static int[] merge(int[] a, int [] b) {
+
+  public static int[] createArray(int[] data, int lo, int hi) {
+    int[] L = new int[hi-lo+1];
+    int i = 0;
+    while (hi > lo) {
+      L[i] = data[lo];
+      lo++;
+    }
+    return L;
+  }
+
+  public static int[] merge(int[] data, int lo, int divide, int hi) {
+    int[] a = createArray(data, lo, divide);
+    int[] b = createArray(data, divide+1, hi);
+
+    //sorting
     int indexA = 0;
     int indexB = 0;
     int i = 0;
@@ -26,16 +41,29 @@ public class Merge {
         indexB++;
       }
       i++;
-      System.out.println(Arrays.toString(complete));
     }
     return complete;
   }
 
+  public static void mergesort(int[]data){
+    mergesort(data, 0, data.length-1);
+  }
+
+  public static void mergesort(int[]data, int lo, int hi){
+    if (lo >= hi) {
+      return;
+    }
+    mergesort(data, lo, lo+hi/2);
+    mergesort(data, lo+hi/2+1, hi);
+    merge(data, lo, lo+hi/2, hi);
+  }
+
   public static void main(String[] args) {
-    int[] A = new int[] {3, 8, 7};
+    int[] A = new int[] {5, 8, 3, 0, 7, 6};
     int[] B = new int[] {5, 6};
 
-    System.out.println(Arrays.toString(merge(A, B)));
+    mergesort(A);
+    System.out.println(Arrays.toString(A));
     //System.out.println(Arrays.toString(merge(B, A)));
   }
 }
