d658f2a7105651f03c2c5db5c07d4a15f07aec84
vlu00 <vlu00@stuy.edu>
2019-03-18 14:49:07 -0400

    Got rid of test print statements and tested. Times are slower so I'm going to try optimizing

--- Merge.java
+++ Merge.java
@@ -4,9 +4,6 @@ import java.util.*;
 public class Merge {
 
   public static void merge(int[] data, int lo, int divide, int hi) {
-    System.out.println(Arrays.toString(data));
-    System.out.println("lo " + lo + " d " + divide + " hi " + hi);
-
     int j = lo;
     int k = divide+1;
     int i = 0;
@@ -25,17 +22,12 @@ public class Merge {
       i++;
       k++;
     }
-    //System.out.println("a " + Arrays.toString(a));
-    //System.out.println("b" + Arrays.toString(b));
 
     //sorting
     int indexA = 0;
     int indexB = 0;
     int h = 0;
     while (h < a.length+b.length) {
-      //System.out.println(lo);
-      //System.out.println(indexA);
-      //System.out.println(indexB);
       if (indexA == a.length) {
         data[lo] = b[indexB];
         indexB++;
@@ -71,6 +63,42 @@ public class Merge {
     merge(data, lo, divide, hi);
   }
 
+  public static void main(String[]args){
+    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+    int[]MAX_LIST = {1000000000,10};
+    for(int MAX : MAX_LIST){
+      for(int size = 31250; size < 2000001; size*=4){
+        long qtime=0;
+        long btime=0;
+        //average of 5 sorts.
+        for(int trial = 0 ; trial <=5; trial++){
+          int []data1 = new int[size];
+          int []data2 = new int[size];
+          for(int i = 0; i < data1.length; i++){
+            data1[i] = (int)(Math.random()*MAX);
+            data2[i] = data1[i];
+          }
+          long t1,t2;
+          t1 = System.currentTimeMillis();
+          mergesort(data2);
+          t2 = System.currentTimeMillis();
+          qtime += t2 - t1;
+          t1 = System.currentTimeMillis();
+          Arrays.sort(data1);
+          t2 = System.currentTimeMillis();
+          btime+= t2 - t1;
+          if(!Arrays.equals(data1,data2)){
+            System.out.println("FAIL TO SORT!");
+            System.exit(0);
+          }
+        }
+        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+      }
+      System.out.println();
+    }
+  }
+
+/*
   public static void main(String[] args) {
     //int[] A = new int[] {0, 3, 5, 6, 7, 8};
     int[] B = new int[100];
@@ -82,4 +110,5 @@ public class Merge {
     //merge(A, 0, 2, 5);
     //System.out.println(A);
   }
+  */
 }
