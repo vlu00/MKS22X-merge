87a4bc85949abd1a641d00a361132488431e6005
vlu00 <vlu00@stuy.edu>
2019-03-21 02:07:36 -0400

    When data length is 40, insertionsort seems to work best (less time for many duplicates). Got rid of extra methods and added comments

--- Merge.java
+++ Merge.java
@@ -31,21 +31,21 @@ public class Merge {
   }
 
   public static void merge(int[] data, int[] temp, int lo, int divide, int hi) {
-    int indexA = 0;
-    int indexB = 0;
+    int indexA = 0; //index of left list being checked
+    int indexB = 0; //index of right list being checked
     int counter = 0;
 
     while (counter <= hi-lo) {
       if (indexA > divide-lo) {
-        data[lo+counter] = temp[divide+indexB+1];
-        indexB++;
+        data[lo+counter] = temp[divide+indexB+1]; //if one "list" is empty
+        indexB++; //keep adding ints of remaining list
       }
       else if (indexB >= hi - divide) {
         data[lo+counter] = temp[lo+indexA];
         indexA++;
       }
-      else if (temp[lo+indexA] < temp[divide+indexB+1]) {
-        data[lo+counter] = temp[lo + indexA];
+      else if (temp[lo+indexA] < temp[divide+indexB+1]) { //checking which next element is least
+        data[lo+counter] = temp[lo + indexA]; //that gets added to data
         indexA++;
       }
       else {
@@ -57,162 +57,19 @@ public class Merge {
   }
 
   public static void mergesort(int[]data){
-    int[] temp = createArray(data, 0, data.length-1);
+    int[] temp = createArray(data, 0, data.length-1); //duplicate list
     mergesort(data, temp, 0, data.length-1);
   }
 
   public static void mergesort(int[]data, int[]temp, int lo, int hi){
-    if (lo >= hi) {
-      return;
+    if (hi-lo+1 < 40) {
+      insertionsort(data, lo, hi); //if data is small enough, use insertionsort (good for many duplicates)
     }
-    int divide = (lo+hi)/2;
-    mergesort(temp, data, lo, divide);
-    mergesort(temp, data, divide+1, hi);
-    merge(data, temp, lo, divide, hi);
-
-  }
-
-  public static void mergeSort(int[]data){
-    mergeSort(data, 0, data.length-1);
-  }
-
-  public static void mergeSort(int[]data, int lo, int hi){
-    if (lo >= hi) {
-      return;
-    }
-    int divide = (lo+hi)/2;
-    mergeSort(data, lo, divide);
-    mergeSort(data, divide+1, hi);
-    merge(data, lo, divide, hi);
-  }
-
-  public static void merge(int[] data, int lo, int divide, int hi) {
-    int j = lo;
-    int k = divide+1;
-    int i = 0;
-
-    int[] a = new int[divide-lo+1];
-    while (j < divide+1) {
-      a[i] = data[j];
-      i++;
-      j++;
-    }
-
-    i = 0;
-    int[] b = new int [hi-divide];
-    while (k < hi+1) {
-      b[i] = data[k];
-      i++;
-      k++;
-    }
-
-    //sorting
-    int indexA = 0;
-    int indexB = 0;
-    int h = 0;
-    while (h < a.length+b.length) {
-      if (indexA == a.length) {
-        data[lo] = b[indexB];
-        indexB++;
-      }
-      else if (indexB == b.length) {
-        data[lo] = a[indexA];
-        indexA++;
-      }
-      else if (a[indexA] < b[indexB]) {
-        data[lo] = a[indexA];
-        indexA++;
-      }
-      else {
-        data[lo] = b[indexB];
-        indexB++;
-      }
-      lo++;
-      h++;
+    else {
+      int divide = (lo+hi)/2;
+      mergesort(temp, data, lo, divide);
+      mergesort(temp, data, divide+1, hi);
+      merge(data, temp, lo, divide, hi); 
     }
   }
-
-  public static void main(String[]args){
-    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
-    int[]MAX_LIST = {1000000000,10};
-    for(int MAX : MAX_LIST){
-      for(int size = 31250; size < 2000001; size*=4){
-        long qtime=0;
-        long btime=0;
-        //average of 5 sorts.
-        for(int trial = 0 ; trial <=5; trial++){
-          int []data1 = new int[size];
-          int []data2 = new int[size];
-          for(int i = 0; i < data1.length; i++){
-            data1[i] = (int)(Math.random()*MAX);
-            data2[i] = data1[i];
-          }
-          long t1,t2;
-          t1 = System.currentTimeMillis();
-          mergesort(data2);
-          t2 = System.currentTimeMillis();
-          qtime += t2 - t1;
-          t1 = System.currentTimeMillis();
-          Arrays.sort(data1);
-          t2 = System.currentTimeMillis();
-          btime+= t2 - t1;
-          if(!Arrays.equals(data1,data2)){
-            System.out.println("FAIL TO SORT!");
-            System.exit(0);
-          }
-        }
-        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
-      }
-      System.out.println();
-    }
-
-    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
-    //int[]MAX_LIST = {1000000000,10};
-    for(int MAX : MAX_LIST){
-      for(int size = 31250; size < 2000001; size*=4){
-        long qtime=0;
-        long btime=0;
-        //average of 5 sorts.
-        for(int trial = 0 ; trial <=5; trial++){
-          int []data1 = new int[size];
-          int []data2 = new int[size];
-          for(int i = 0; i < data1.length; i++){
-            data1[i] = (int)(Math.random()*MAX);
-            data2[i] = data1[i];
-          }
-          long t1,t2;
-          t1 = System.currentTimeMillis();
-          mergeSort(data2);
-          t2 = System.currentTimeMillis();
-          qtime += t2 - t1;
-          t1 = System.currentTimeMillis();
-          Arrays.sort(data1);
-          t2 = System.currentTimeMillis();
-          btime+= t2 - t1;
-          if(!Arrays.equals(data1,data2)){
-            System.out.println("FAIL TO SORT!");
-            System.exit(0);
-          }
-        }
-        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
-      }
-      System.out.println();
-    }
-  }
-
 }
-/*
-
-
-public static void main(String[] args) {
-  int[] A = new int[] {4, 3, 2, 1};
-  //int[] B = new int[100];
-  //for (int i = 0; i < 100; i++) {
-  //  B[i] = 100-i;
-  //}
-  mergesort(A);
-  System.out.println(Arrays.toString(A));
-  //merge(A, 0, 2, 5);
-  //System.out.println(A);
-}
-*/
